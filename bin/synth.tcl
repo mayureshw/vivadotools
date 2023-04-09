3e0b9799bfee8de9ada8cb2e96031014ff648b33
mayuresh@wdc <mayuresh@wdc>
2023-04-09 12:48:11 +0530

    PARTNAME specification added

--- bin/synth.tcl
+++ bin/synth.tcl
@@ -1,10 +1,17 @@
-set SRCS $env(SRCS)
-set TOP  $env(TOP)
-set DCP  $TOP
+set SRCS        $env(SRCS)
+set TOP         $env(TOP)
+set PARTNAME    $env(PARTNAME)
+set DCP         $TOP
+
 append DCP .synth.dcp
 
 add_files $SRCS
 set_property top $TOP [get_filesets sources_1]
-synth_design -rtl
-synth_design
+if { $PARTNAME != "" } {
+    synth_design -rtl -part $PARTNAME
+    synth_design -part $PARTNAME
+} else {
+    synth_design -rtl
+    synth_design
+    }
 write_checkpoint -force $DCP
