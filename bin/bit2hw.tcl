f1907f55f48eca51621a84edd4e807eb7f577faf
guest@lenovo <guest@lenovo>
2023-04-09 21:43:51 +0530

    bit2hw : minor modifications

--- bin/bit2hw.tcl
+++ bin/bit2hw.tcl
@@ -1,10 +1,10 @@
 set TOP  $env(TOP)
 set BITSTREAMFILE  $TOP
 append BITSTREAMFILE .bit
-open_hw
+open_hw_manager
 connect_hw_server
 # This is not found to be necessary, though in its absence if sometimes
 # says "target not connected", but still transfers the bistream
-open_hw_target
+#open_hw_target
 set_property PROGRAM.FILE $BITSTREAMFILE [current_hw_device]
 program_hw_devices
