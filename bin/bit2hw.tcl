e12d16a3a8d34e1835613e46659c11c3a3631e1b
guest@lenovo <guest@lenovo>
2023-04-10 21:25:09 +0530

    minor change: a line uncommented

--- bin/bit2hw.tcl
+++ bin/bit2hw.tcl
@@ -5,6 +5,6 @@ open_hw_manager
 connect_hw_server
 # This is not found to be necessary, though in its absence if sometimes
 # says "target not connected", but still transfers the bistream
-#open_hw_target
+open_hw_target
 set_property PROGRAM.FILE $BITSTREAMFILE [current_hw_device]
 program_hw_devices
