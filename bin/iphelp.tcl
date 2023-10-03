5ac577cd62f8f4cc86049da21b209aadb89b6d24
mayuresh@wdc <mayuresh@wdc>
2023-10-03 17:37:22 +0530

    validation sequence changed

--- bin/iphelp.tcl
+++ bin/iphelp.tcl
@@ -1,8 +1,8 @@
-set VLNV [lindex $argv 0]
 if { $argc != 1 } {
     puts "Pass vlnv string of the ip as an argument"
     exit
     }
+set VLNV [lindex $argv 0]
 create_project -in_memory
 create_ip -vlnv $VLNV -module_name iphelp
 puts [report_property -all [get_ips]]
