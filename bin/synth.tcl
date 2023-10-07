3709d67f6d8b82ca71110dc75b4cdea791040dbe
mayuresh@wdc <mayuresh@wdc>
2023-10-07 12:58:39 +0530

    synthesis ips from xci files supported

--- bin/synth.tcl
+++ bin/synth.tcl
@@ -9,11 +9,14 @@ foreach FILE $env(SRCS) {
     set EXT [ file extension $FILE ]
     if { $EXT == ".vhdl" } {
         read_vhdl -vhdl2008 $FILE
+    } elseif { $EXT == ".xci" } {
+        read_ip $FILE
     } else {
         add_files $FILE
     }
 }
 
+synth_ip [get_ips]
 set_property top $TOP [get_filesets sources_1]
 if { $PARTNAME != "" } {
     synth_design -rtl -part $PARTNAME
