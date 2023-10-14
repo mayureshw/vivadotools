e0961d9671a34bca97bde9b4c12ed21314f795a7
mayuresh@wdc <mayuresh@wdc>
2023-10-14 11:32:13 +0530

    added a notion of IPSRCDIR to ease forced write

--- bin/ipgen.tcl
+++ bin/ipgen.tcl
@@ -9,10 +9,12 @@ if { $argc < 3 } {
 set VLNV [lindex $argv 0]
 set TGT [lindex $argv 1]
 set MODULE [lindex $argv 2]
+set IPSRCDIR "ipsrc"
+file mkdir $IPSRCDIR
 create_project -in_memory
-create_ip -vlnv $VLNV -module_name $MODULE
+create_ip -dir $IPSRCDIR -force -vlnv $VLNV -module_name $MODULE
 set PROPVALS [lrange $argv 3 end]
 if { $argc > 3 } {
     set_property -dict "$PROPVALS" [get_ips]
 }
-generate_target $TGT [get_ips]
+generate_target -force $TGT [get_ips]
